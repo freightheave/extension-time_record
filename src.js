41efd9cbd9f22215e99a35fdf21d065f8976b28e
Roshan Komaravolu <17759116+freightheave@users.noreply.github.com>
2024-03-04 15:08:03 +0530

    refactoring

--- src.js
+++ src.js
@@ -1,26 +1,20 @@
 const hostname = window.location.hostname;
 //www.reddit.com
-if (hostname === 'www.twitch.tv'){ 
-    const timeLimit = 100; // Limit
-    // Get the current timestamp
-    const startTime = Date.now();
+if (hostname === 'www.twitch.tv') { 
+    const timeLimit = 300; // Limit
+    const startTime = Date.now();     // Get the current timestamp
 
-    // Store the start time in localStorage
     localStorage.setItem('startTime', startTime);
 
-    // Check the elapsed time periodically
-    const checkElapsedTime = () => {
+    const checkElapsedTime = () => {     // Check the elapsed time periodically
     const currentTime = Date.now();
     const elapsedTime = (currentTime - startTime) / 1000; // Convert to seconds
 
-        if (elapsedTime >= timeLimit) {
-            // Redirect to another page (e.g., "https://example.com")
+        if (elapsedTime >= timeLimit) { // Redirect to another page
             window.location.href = 'file:///D:/Repositories/extension/Redirect.html';
         } else {
-            // Continue checking
             setTimeout(checkElapsedTime, 1000); // Check every second
         }
     };
 }
-// Start checking elapsed time
-checkElapsedTime();
+checkElapsedTime(); // Start checking elapsed time
